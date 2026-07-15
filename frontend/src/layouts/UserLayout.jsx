ea7fe6203c7b262a2a8b1152273517137593ba0d
Aung Min Khant <89652503+AungMinKhant123@users.noreply.github.com>
2026-07-15 12:22:31 +0630

    feat: frontend setup

---
+++ frontend/src/layouts/UserLayout.jsx
@@ -0,0 +1,23 @@
+import { Routes, Route } from "react-router";
+import React from "react";
+import Header from "../components/user/Header";
+import Footer from "../components/user/Footer";
+import Home from "../pages/user/Home";
+import AboutUs from "../pages/user/AboutUs";
+
+const UserLayout = () => {
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about-us" element={<AboutUs />} />
+        </Routes>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default UserLayout;
